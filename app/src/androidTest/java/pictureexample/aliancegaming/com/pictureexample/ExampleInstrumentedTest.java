9b643826c7e480e923e0cd14f441aed4e92ad9d5
owenoliver1 <owenoliver1@gmail.com>
2021-09-20 17:48:18 +0100

    Fixing the tests

--- app/src/androidTest/java/pictureexample/aliancegaming/com/pictureexample/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/pictureexample/aliancegaming/com/pictureexample/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("pictureexample.aliancegaming.com.updatetheapp", appContext.getPackageName());
+        assertEquals("com.aliancegaming.pictureexample", appContext.getPackageName());
     }
 }
